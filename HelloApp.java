a640d272cde9dd450e3efb42ed62fa78a2822565
as9766 <as9766@srmist.edu.in>
2026-03-18 00:49:58 +0530

    Coded UC7: Display initials from full name

--- HelloApp.java
+++ HelloApp.java
@@ -1,19 +1,10 @@
 public class HelloApp {
-
     public static void main(String[] args) {
-
-        if (args.length < 2) {
-            System.out.println("Please provide first and last name");
-            return;
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String names = String.join(" ", args);
+            System.out.println("Hello, " + names + "!");
         }
-
-        String firstName = args[0];
-        String lastName = args[1];
-
-        // Get initials
-        char firstInitial = firstName.charAt(0);
-        char lastInitial = lastName.charAt(0);
-
-        System.out.println("Initials: " + firstInitial + "." + lastInitial);
     }
 }
