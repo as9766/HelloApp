292d0709d43dd26aeaecd5683bf812c7ba80268c
as9766 <as9766@srmist.edu.in>
2026-03-18 00:54:26 +0530

    Coded UC5: Display Hello using enhanced for loop

--- HelloApp.java
+++ HelloApp.java
@@ -1,10 +1,16 @@
 public class HelloApp {
     public static void main(String[] args) {
+
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String names = String.join(" ", args);
-            System.out.println("Hello, " + names + "!");
+            String result = "";
+
+            for (String name : args) {
+                result += name + " ";
+            }
+
+            System.out.println("Hello, " + result.trim() + "!");
         }
     }
 }
